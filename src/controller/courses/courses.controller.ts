c5a0408459e13819f6f13ca7e22a54658bcb54b7
JoaoVitorLima242 <jlima0500@gmail.com>
2023-04-05 11:42:27 -0300

    patch & delete in nest

--- src/controller/courses/courses.controller.ts
+++ src/controller/courses/courses.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
   Res,
 } from '@nestjs/common';
@@ -26,5 +28,15 @@ export class CoursesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atuslização curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Deletando curso #${id}`;
   }
 }
