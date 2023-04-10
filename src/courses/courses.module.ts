b488934a1352e8053a98d1ff8d2a95626d6a791b
JoaoVitorLima242 <jlima0500@gmail.com>
2023-04-10 15:20:04 -0300

    add typeorm entity configuration

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CoursesController } from './courses.controller';
 import { CourseService } from './course.service';
+import { Course } from './entities/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CoursesController],
   providers: [CourseService],
 })
